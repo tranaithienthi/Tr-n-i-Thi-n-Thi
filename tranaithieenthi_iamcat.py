71455f27bfed0d09ba48244d218cbccce69f2f51
tranaithienthi <thienthi2312@gmail.com>
2024-08-25 11:45:02 +0700

    Update tranaithieenthi_iamcat.py

--- tranaithieenthi_iamcat.py
+++ tranaithieenthi_iamcat.py
@@ -4,5 +4,8 @@ Created on Sun Aug 25 10:21:29 2024
 
 @author: Student
 """
-chuoi=("i'm a cat")
-print(chuoi.replace("i", "I")).replace("a", "A")
+chuoi="i'm a cat"
+chuoi1=chuoi.replace("i", "I").replace("a","A", 1).replace("c", "C")
+print(chuoi1)
+chuoi2=chuoi.upper()
+print(chuoi2)

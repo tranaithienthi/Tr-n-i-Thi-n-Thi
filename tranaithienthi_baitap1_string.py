22146c5c92c20d780c7df26f1756a0dfaa798a1f
tranaithienthi <thienthi2312@gmail.com>
2024-08-25 13:45:48 +0700

    Update tranaithienthi_baitap2.1

--- tranaithienthi_baitap1_string.py
+++ tranaithienthi_baitap1_string.py
@@ -5,15 +5,8 @@ Created on Sun Aug 25 09:57:32 2024
 @author: Student
 """
 
-var= "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
-print("+", var[:16])
-print("+", var[18:27])
-print("+", var[29:42])
-print("+", var[44:54])
-print("+", var[55:63])
-
-print("+", var[:16])
-print("+", var[18:27])
-print("+", var[31:42])
-print("+", var[46:54])
-print("+", var[59:63])
+chuoi="Đại học quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
+chuoi1=chuoi.split(" , ")
+chuoi2=chuoi.replace("P. ","").replace("Q. ","").split(",")
+print(chuoi1)
+print(chuoi2)

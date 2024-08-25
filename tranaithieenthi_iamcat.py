56fcc35cd6015d92dbbac369bacbd1926c397249
tranaithienthi <thienthi2312@gmail.com>
2024-08-25 13:44:47 +0700

    Update tranaithieenthi_iamcat.py

--- tranaithieenthi_iamcat.py
+++ tranaithieenthi_iamcat.py
@@ -4,8 +4,12 @@ Created on Sun Aug 25 10:21:29 2024
 
 @author: Student
 """
+
 chuoi="i'm a cat"
 chuoi1=chuoi.replace("i", "I").replace("a","A", 1).replace("c", "C")
 print(chuoi1)
 chuoi2=chuoi.upper()
 print(chuoi2)
+chuoi3=chuoi.replace("m", "M").replace("at", "AT")
+print(chuoi3)
+print(chuoi.capitalize())

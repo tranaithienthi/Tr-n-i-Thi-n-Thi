130345661f9bf2bd95cd0a4379d3faa31d3bb268
tranaithienthi <thienthi2312@gmail.com>
2024-08-25 13:43:44 +0700

    Update tranaithienthi_baitap1.2.py

--- tranaithienthi_baitap1.2.py
+++ tranaithienthi_baitap1.2.py
@@ -11,5 +11,7 @@ b=int(input("Nhập số nguyên b:"))
 print("Tổng của a,b là:", a+b)
 print("Hiệu của a,b là:", a-b)
 print("Tích của a,b là:", a*b)
+s=a/b
+print("Thương của a,b là:", round(s,3))
 print("Chia lấy dư của a,b là:", a%b)
 print("Chia lấy nguyên của a,b là:", a//b)
